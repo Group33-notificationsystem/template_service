87e29555fe98a769f2531fce6427816db7682216
Benjamin Odion <BenjaminOwase@gmail.com>
2025-11-14 15:04:12 +0100

    Add SQLite database

--- template_service/settings.py
+++ template_service/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = "template_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'template_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Ben123',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
